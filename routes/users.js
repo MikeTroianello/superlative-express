4450700743563fb766b84e73926b2848ce066076
Mike <troianello@protonmail.com>
2022-05-20 00:06:10 -0400

    fetch users

--- routes/users.js
+++ routes/users.js
@@ -46,4 +46,18 @@ router.post("/login", async (req, res) => {
   }
 });
 
+router.post("/all-other-users", async (req, res) => {
+  try {
+    let users = await User.find();
+
+    let notYou = users.filter((user) => {
+      return user._id !== req.user._id;
+    });
+
+    res.json(notYou);
+  } catch (err) {
+    res.status(400).json(err.message);
+  }
+});
+
 module.exports = router;
